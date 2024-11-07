18f5d3d29b28da00474a13fbe841ff4ce015b1ff
dawidciaston <94467549+Kapciur@users.noreply.github.com>
2024-11-07 21:20:06 +0100

    Delete zad2 directory

--- zad2/square.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "rectangle.hpp"
-
-class Square : public Rectangle {
-public:
-    // parametrized constructor
-    Square(double a, const Point &p) : Rectangle(a, a, p, "square") {}
-};
